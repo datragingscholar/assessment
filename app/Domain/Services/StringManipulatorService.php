e48e416e06f09e366bf9113d39250e9c00dbe970
ragingscholar <ragingscholar@protonmail.com>
2020-01-10 17:03:26 +0800

    Implemented toAlternateCase(), test passed

--- app/Domain/Services/StringManipulatorService.php
+++ app/Domain/Services/StringManipulatorService.php
@@ -17,4 +17,26 @@ class StringManipulatorService
     {
         return mb_convert_case($string, MB_CASE_UPPER, 'UTF-8');
     }
+
+    public static function toAlternateCase(StringEntity $stringEntity) : void
+    {
+        $characters = str_split($stringEntity->get());
+        //var_dump($characters);
+        $result = '';
+        foreach ($characters as $key => $character) {
+            if ($key % 2 == 0) {
+                $result .= self::toLowerCase($character);
+                continue;
+            }
+
+            $result .= self::toUpperCase($character);
+        }
+
+        $stringEntity->updateString($result);
+    }
+
+    protected static function toLowerCase(String $string) : String
+    {
+        return mb_convert_case($string, MB_CASE_LOWER, 'UTF-8');
+    }
 }
