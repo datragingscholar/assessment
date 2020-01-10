a52da4ae8cf4b028b8ccd9545c84ae052b4bdbd8
ragingscholar <ragingscholar@protonmail.com>
2020-01-10 17:36:36 +0800

    Added test_can_initialize_and_get_path()

--- tests/Unit/Persistance/CSVPersistanceTest.php
+++ tests/Unit/Persistance/CSVPersistanceTest.php
@@ -1,9 +1,24 @@
 <?php
 
-namespace Tests\Persistance;
+namespace Tests\Unit\Persistance;
 
 use Tests\TestCase;
+use App\Persistance\CSVPersistance;
 
 class CSVPersistanceTest extends TestCase
 {
+    /**
+     * @test
+     * @covers CSVPersistance::currentPath
+     */
+    public function test_can_initialize_and_get_path()
+    {
+        $path = getcwd() . '/test.csv';
+        $csvPersistance = new CSVPersistance(
+            new StringFactory,
+            $path
+        );
+
+        $this->assertEquals($path, $csvPersistance->currentPath());
+    }
 }
