6e0a53104de07d88d581573293d9cfaa4262fd44
ragingscholar <ragingscholar@protonmail.com>
2020-01-10 16:10:32 +0800

    Added test_can_intialize_and_get_string to StringEntityTest.

--- tests/Unit/Domain/Entities/StringEntityTest.php
+++ tests/Unit/Domain/Entities/StringEntityTest.php
@@ -3,7 +3,19 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\Domain\Entities\StringEntity;
 
 class StringEntityTest extends TestCase
 {
+    /**
+     * @test
+     * @covers StringEntity::get
+     */
+    public function test_can_initialize_and_get_string()
+    {
+        $stringEntity = new StringEntity('test string');
+
+        $this->assertIsString($stringEntity->get());
+        $this->assertEquals('test string', $stringEntity->get());
+    }
 }
