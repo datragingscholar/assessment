8d38a2ee5607989d6c47bd6fc1e68387ef5bafa6
ragingscholar <ragingscholar@protonmail.com>
2020-01-10 16:13:44 +0800

    Added constructor and get() method to pass test

--- app/Domain/Entities/StringEntity.php
+++ app/Domain/Entities/StringEntity.php
@@ -4,5 +4,15 @@ namespace App\Domain\Entities;
 
 class StringEntity
 {
+    protected $string = '';
 
+    public function __construct(String $string)
+    {
+        $this->string = $string;
+    }
+
+    public function get() : String
+    {
+        return $this->string;
+    }
 }
