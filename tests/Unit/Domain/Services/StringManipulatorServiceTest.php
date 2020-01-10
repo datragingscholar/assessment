cf794310dc1c88651d9bb10f61e41ba10cc48ea4
ragingscholar <ragingscholar@protonmail.com>
2020-01-10 16:34:14 +0800

    Added test_can_correctly_convert_to_uppercase()

--- tests/Unit/Domain/Services/StringManipulatorServiceTest.php
+++ tests/Unit/Domain/Services/StringManipulatorServiceTest.php
@@ -1,5 +1,20 @@
 <?php
 
-class StringManipulatorService
+namespace Tests\Unit\Domain\Services;
+
+use Tests\TestCase;
+use App\Domain\Entities\StringEntity;
+
+class StringManipulatorService extends TestCase
 {
+    /**
+     * @test
+     * @covers StringManipulator::toAllUpperCase
+     */
+    public function test_can_correctly_convert_to_uppercase()
+    {
+        $string = new StringEntity('hello world');
+        StringManipulator::toAllUpperCase($string);
+        $this->assertEquals('HELLO WORLD', $string->get());
+    }
 }
